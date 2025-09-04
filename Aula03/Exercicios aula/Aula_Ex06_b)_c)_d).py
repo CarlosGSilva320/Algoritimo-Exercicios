7ba33060896d8747e96d19165a27ab1a6ae638fd
CarlosGSilva320 <car_gsilva@yahoo.com.br>
2025-09-03 21:16:17 -0300

    atualização

---
+++ Aula03/Exercicios aula/Aula_Ex06_b)_c)_d).py
@@ -0,0 +1,17 @@
+#6 -
+#b) Crie um dicionário de notas de 3 alunos (nome como chave, nota como valor). 
+#c) Acesse a nota de um dos alunos e exiba. 
+#d) Remova um aluno do dicionário de notas.
+
+
+turma = {
+    "aluno1" : {"carlos" : 10},
+    "aluno2" : {"allan": 9},
+    "aluno3" : {"daniel": 8}
+
+}
+
+print(turma["aluno1"]["carlos"])
+turma.pop("aluno2")
+
+print(turma)
